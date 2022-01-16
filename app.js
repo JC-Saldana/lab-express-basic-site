a035129f523b24831ea321bdb7b998012953dde5
Juan Carlos Saldaña Herrero <jcarlossaldae@gmail.com>
2022-01-16 13:00:28 +0100

    finished

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+
+app.use(express.static("public"));
+
+app.get("/home", (req, res) => {
+    res.sendFile(__dirname + "/views/home.html")
+})
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/views/about.html")
+})
+app.get("/works", (req, res) => {
+    res.sendFile(__dirname + "/views/works.html")
+})
+app.use((req, res) => {
+    res.status(404).send("Not found wey");
+  });
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000 🚀");
+  });
+
